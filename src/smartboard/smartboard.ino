5ec086924c2cee861a13cd8e6e86d5de5fefdfe0
Suhel Mujawar <suhelmujawar3369@gmail.com>
2026-02-02 12:36:31 +0530

    Relay configuration and testing.

--- src/smartboard/smartboard.ino
+++ src/smartboard/smartboard.ino
@@ -1,17 +1,32 @@
 // Author: Suhel Mujawar
+/*Note: In NodeMCU HIGH -> 0 and LOW -> 1*/
 
 #include <ESP8266WiFi.h>
 #include "config.h"
 
+// Relay configuration
+// Defining pins
+#define RELAY1 D1
+#define RELAY2 D2
+
 void setup() {
   // put your setup code here, to run once:
+  // Configuring relay pins
+  pinMode(RELAY1, OUTPUT);
+  pinMode(RELAY2, OUTPUT);
+
   pinMode(LED_BUILTIN, OUTPUT);
+  
+  // Keeping relay OFF at startup
+  digitalWrite(RELAY1, HIGH);
+  digitalWrite(RELAY2, HIGH);
+
   Serial.begin(115200);
 
   Serial.println();
   Serial.println("Connecting to WiFi");
 
-  // Establish WiFi connection
+  // Establish WiFi connection(tested)
   WiFi.begin(WIFI_SSID,WIFI_PASSWORD);
 
    // Establishing WiFi connection
@@ -33,4 +48,13 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  // For testing relays :
+  Serial.println("Relay1 ON.")
+  digitalWrite(RELAY1, LOW);
+  delay(1000);
+  digitalWrite(RELAY1, HIGH);
+  Serial.println("Relay2 ON.")
+  digitalWrite(RELAY2, LOW);
+  delay(1000);
+  digitalWrite(RELAY2, HIGH);
 }
