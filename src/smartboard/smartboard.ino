50adb1e35eb01511361ddab46de95d00d9ef1e9d
Suhel Mujawar <suhelmujawar3369@gmail.com>
2026-02-02 20:50:51 +0530

    Initial Firebase setup with relay testing.

--- src/smartboard/smartboard.ino
+++ src/smartboard/smartboard.ino
@@ -2,12 +2,19 @@
 /*Note: In NodeMCU HIGH -> 0 and LOW -> 1*/
 
 #include <ESP8266WiFi.h>
+#include <Firebase.h>
 #include "config.h"
 
 // Relay configuration
 // Defining pins
 #define RELAY1 D1
 #define RELAY2 D2
+
+// Firebase Objects
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -44,17 +51,42 @@ void setup() {
   Serial.print("WiFi IP:");
   Serial.println(WiFi.localIP());
 
+  // Firebase code
+  Serial.println("Connecting Firebase.");
+  // database configuration
+  config.api_key = API_KEY;
+  config.database_url = DATABASE_URL;
+
+  //authentication configuration
+  auth.user.email = FIREBASE_EMAIL;
+  auth.user.password = FIREBASE_PASSWORD;
+
+  // Connecting firebase
+  Firebase.begin(&config, &auth);
+
+  Firebase.reconnectWiFi(true);
+  Serial.println("Firebase connected.");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  // For testing relays :
-  Serial.println("Relay1 ON.")
-  digitalWrite(RELAY1, LOW);
-  delay(1000);
-  digitalWrite(RELAY1, HIGH);
-  Serial.println("Relay2 ON.")
-  digitalWrite(RELAY2, LOW);
-  delay(1000);
-  digitalWrite(RELAY2, HIGH);
+  if(Firebase.RTDB.getInt(&fbdo, "smartboard/devices/appliance1/state")){
+    int state = fbdo.intData();
+    if(state == 1){
+      digitalWrite(RELAY1, LOW);
+    }
+    else{
+      digitalWrite(RELAY1, LOW);  
+    }
+
+    else{
+      Serial.println(fbdo.errorReason());
+    }
+
+    delay(350);// this avoid excessive firbase reads
+  }
 }
+
+// Important Note :
+/* Later i am going to upgrade to Firebase streaming from this
+ traditional without streaming (polling) method.*/
